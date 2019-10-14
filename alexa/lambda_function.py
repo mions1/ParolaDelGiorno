e970d1f7c5958739d03e1712074f0f9e9df21253
Simone <simone.mione1@gmail.com>
2019-10-14 17:47:28 +0200

    Passaggio a classi anche la versione per alexa.

--- alexa/lambda_function.py
+++ alexa/lambda_function.py
@@ -19,6 +19,11 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import urllib.request
 
+from handle_source import source_handler
+from handle_source.zanichelli import zanichelli as zanni
+from handle_source.treccani import treccani as trec
+from handle_source.virgilio import virgilio as virgi
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -50,9 +55,21 @@ class MainIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        word, mean = self.main()
-        speak_output = "La parola di oggi è: "+word
-        speak_output += "\nChe significa: "+mean
+        word, mean, sins, cons = self.main()
+        
+        if word != "":
+            speak_output = "La parola di oggi è: "+word+"."
+            if mean != "":
+                speak_output += "\nChe significa: "+mean
+            else:
+                speak_output += "\nLa definizione non è disponibile."
+            if sins != "":
+                speak_output += "\nAlcuni sinonimi sono: "+sins+"."
+            if cons != "":
+                speak_output += "\nAlcuni contrari sono: "+cons+"."
+        else:
+            speak_output = "Oggi la parola non è disponibile!"
+        
         return (
             handler_input.response_builder
                 .speak(speak_output)
@@ -63,109 +80,25 @@ class MainIntentHandler(AbstractRequestHandler):
     def main(self):
         #-----------ZANICHELLI for word
         url = "https://dizionaripiu.zanichelli.it/cultura-e-attualita/le-parole-del-giorno/parola-del-giorno/"
-        source = self.get_source(url)
-        main_source = self.get_main_source(source)
-        opt_main_source = self.delete_more_digit(self.special(self.delete_tag(main_source)))
+        zan = zanni.Zanichelli(url)
+        zan.init()
         
-        word = self.get_word(opt_main_source)
+        word = zan.word
         #------------TRECCANI for meaning
         url = "http://www.treccani.it/vocabolario/"+word+"/"
-        source = self.get_source(url)
-        main_source = self.get_main_source(source, "treccani")
+        tre = trec.Treccani(url, word)
+        tre.init()
         
-        mean = self.get_meaning(self.delete_tag(main_source), "treccani")
+        mean = tre.mean
+        #------------VIRGILIO for sins e cons
+        url = "https://sapere.virgilio.it/parole/sinonimi-e-contrari/"+word
+        vir = virgi.Virgilio(url, word)
+        vir.init()
         
-        return word, mean
-    
-    def delete_tag(self, txt):
-        tmp = ""
-        start = False
-        for c in txt:
-            if c == "<":
-                start = True
-                continue
-            if c == ">":
-                start = False
-                continue
-            if start == False:
-                tmp += c
-        return tmp
-
-    def special(self, txt):
-        txt = txt.replace("&ograve;", "ò")
-        txt = txt.replace("&egrave;", "è")
-        txt = txt.replace("&igrave;", "ì")
-        txt = txt.replace("&ugrave;", "ù")
-        return txt
-    
-    def delete_more_digit(self, txt):
-        i = 0
-        last_digit = None
-        tmp_txt = ""
+        sins = vir.sins
+        cons = vir.cons
         
-        for c in txt:
-            if c.isdigit():
-                i += 1
-                last_digit = c
-            else:
-                if i == 1:
-                    tmp_txt += last_digit
-                tmp_txt += c
-                i = 0
-            
-        return tmp_txt
-                
-    def get_source(self, url):
-        with urllib.request.urlopen(url) as response: 
-            source = response.read().decode('utf-8')
-        return source
-    
-    def get_main_source(self, source, src="zanichelli"):
-        main_start = main_end = ""
-        
-        if src == "zanichelli":
-            main_start = '<div class="main-content light-txt">'
-            main_end = '</div>'
-        elif src == "treccani":
-            main_start = '<!-- module article full content -->'
-            main_end = '<!-- end module -->'
-        
-        main_source = source[source.index(main_start):(source[source.index(main_start):]).index(main_end)+len(source[:source.index(main_start)])+len(main_end)]
-        return main_source
-        
-    
-    def get_word(self, main_source):
-        word_start = 'La parola di oggi è: '
-        word_end = '\n'
-        i = 2 if main_source.index(word_end) == 1 else 1
-        word = main_source[main_source.index(word_start)+len(word_start):main_source.index(word_end,i)]
-        return word
-    
-    def get_meaning(self, main_source, src="zingarelli"):
-        special_char = ["|",";",".","(","[","#"]
-        mean = ""
-        if src == "zingarelli":
-            main_start = '1 '
-            tmp = main_source[main_source.index(main_start)+len(main_start):]
-            for c in tmp:
-                if c in  special_char:
-                    break
-                mean += c
-        elif src == "treccani":
-            trattino = upper = False
-            for c in main_source:
-                if trattino:
-                    if upper:
-                        if c in special_char:
-                            break
-                        mean += c
-                    elif c.isupper():
-                        upper = True
-                        mean += c
-                elif "–" in c:
-                    trattino = True
-        
-        return mean
+        return word, mean, sins, cons
 
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
---
+++ python/index.rst
@@ -0,0 +1,20 @@
+.. Parola del Giorno documentation master file, created by
+   sphinx-quickstart on Sun Oct 13 23:49:23 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Parola del Giorno's documentation!
+=============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
